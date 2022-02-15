3f6c3ca06087c2e750170410386c3b6762ccab31
Peter <41585899+pbanigo@users.noreply.github.com>
2022-02-15 20:44:17 +0000

    3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	const split = string.split("");
+	let reversed = [];
+	for ( i = split.length; i >= 0; i--) {
+		reversed.push(split[i]);
+	}
+	return reversed.join("");
 
 };
-
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
