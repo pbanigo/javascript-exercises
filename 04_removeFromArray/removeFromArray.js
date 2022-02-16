95c798c2936cd32e3d741604704628fd3ff6b663
Peter <41585899+pbanigo@users.noreply.github.com>
2022-02-16 07:08:10 +0000

    4 Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...elements) {// ... rest parameter for unspecified number of arguments
+	//initialise new array
+	let arr = elements[0];
+    let newArr = [];
+	//search old array with element
+   arr.forEach((element) => {
+    if (!elements.includes(element)) {
+      newArr.push(element);
+    }
+  });
+	//if element not equal to array item, push array item to new array 
+	return newArr;
 };
 
 // Do not edit below this line
