5ccd02cbb2a98787cab3802f017359001bcd41f3
Peter <41585899+pbanigo@users.noreply.github.com>
2022-02-16 12:36:10 +0000

    5th Working

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first,last) {
+  if (first <0 || last < 0 || !Number.isInteger(first) || !Number.isInteger(last)){
+    return "ERROR";
+  }  else {
+    if(first>last){
+      let newOne = first;//save a new variable for first
+      first = last;
+      last = newOne;
+    }
+	let sum = 0;
+	for (let i = first; i <= last; i++) {
+      //console.log(i);
+		sum += i;
+	}
+	return sum;}
 };
 
-// Do not edit below this line
+//Do not edit below this line
 module.exports = sumAll;
